0460fe3ad8be2555eef2c3c1e13816423c5fb4c3
elasri21 <islame.edu@gmail.com>
2022-05-15 15:11:57 +0100

    fix a bug

--- main.js
+++ main.js
@@ -1,44 +1,44 @@
 //
 $(document).ready(function(){
-    const closeBtn = document.querySelector('.close')
-const openBtn = document.querySelector('.open')
-const mobileNav = document.querySelector('nav')
-openBtn.addEventListener('click', function(){
-$(this).css({display: "none"})
-mobileNav.style.display = 'block'
-})
-closeBtn.addEventListener('click', function(){
-    openBtn.style.display = 'block'
-    mobileNav.style.display = 'none'
-})
-let btns = $('.btn')
-let people = document.querySelectorAll('.people .person')
-for(let i=0; i<btns.length; i++){
-    btns[i].addEventListener('click', function(){
-        $('.btn').css({backgroundColor: 'hsl(0, 0%, 100%)'})
-        $(this).css({backgroundColor: 'hsl(12, 88%, 59%)'})
-        for(let j=0;j<people.length; j++){
-            people[j].style.display = 'none';
-            if(j===i){
-                people[j].style.display = 'block';
-            }
-        }
-
+    const closeBtn = document.querySelector('.close');
+    const openBtn = document.querySelector('.open');
+    const mobileNav = document.querySelector('nav');
+    openBtn.addEventListener('click', function(){
+    $(this).css({display: "none"})
+    mobileNav.style.display = 'block'
     })
+    closeBtn.addEventListener('click', function(){
+        openBtn.style.display = 'block'
+        mobileNav.style.display = 'none'
+    })
+    let btns = $('.btn')
+    let people = document.querySelectorAll('.people .person')
+    for(let i=0; i<btns.length; i++){
+        btns[i].addEventListener('click', function(){
+            $('.btn').css({backgroundColor: 'hsl(0, 0%, 100%)'})
+            $(this).css({backgroundColor: 'hsl(12, 88%, 59%)'})
+            for(let j=0;j<people.length; j++){
+                people[j].style.display = 'none';
+                if(j===i){
+                    people[j].style.display = 'block';
+                }
+            }
 
-}
-let form = document.querySelector('form')
-let field = document.querySelector('#email')
-let error = document.querySelector('.error')
-form.addEventListener('submit', function(e){
-    if(field.value === ''){
-        e.preventDefault()
-        error.style.opacity = '1'
+        })
+
     }
-});
-field.addEventListener('focus', function(){
-    error.style.opacity = '0'
-});
+    let form = document.querySelector('form')
+    let field = document.querySelector('#email')
+    let error = document.querySelector('.error')
+    form.addEventListener('submit', function(e){
+        if(field.value === ''){
+            e.preventDefault()
+            error.style.opacity = '1'
+        }
+    });
+    field.addEventListener('focus', function(){
+        error.style.opacity = '0'
+    });
 })
 
 
